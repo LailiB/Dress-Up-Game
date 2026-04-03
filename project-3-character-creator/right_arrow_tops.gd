8752d2bed26372c5f3358d27448774a02823117c
LailiB <lailibul@udel.edu>
2026-04-03 15:14:44 -0400

    Update right_arrow_tops.gd

--- project-3-character-creator/right_arrow_tops.gd
+++ project-3-character-creator/right_arrow_tops.gd
@@ -2,7 +2,7 @@ extends Button
 @export var mybody = [$"../background/body"]
 #@export var myeyes = [$"../background/eyes", $"../background/eyes/eyes (lashes)"]
 @export var mytops = [$"../background/tops/pikminshirt", $"../background/tops/deltashirt", $"../background/tops/omorishirt"]
-
+var cosmetic_on 
 var tracking = 0 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,3 +13,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_pressed() -> void:
+	pass # Replace with function body.
